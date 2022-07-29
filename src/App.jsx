e968cbf88ab3593d2b3ff9d8b2ae1dc125eace9c
Igor-sam-svoj-rob <ijevremovic@yahoo.com>
2022-07-29 11:24:39 +0200

    Router

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Pocetna from "./components/Pocetna";
+import Lokacija from "./components/Lokacija";
+import Kontakt from "./components/Kontakt";
+import Navigacija from "./Navigacija";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigacija />}>
+            <Route index element={<Pocetna />} />
+            <Route path="lokacija" element={<Lokacija />} />
+            <Route path="kontakt" element={<Kontakt />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Pocetna.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Pocetna = () => {
+  return (
+    <div>
+      <h1>Pocetna</h1>
+      <Link to="/kontakt">Kontakt</Link>
+    </div>
+  );
+};
+
+export default Pocetna;
